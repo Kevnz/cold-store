3f85171bce80c9eba84ff245ead33d7e26ad1dfe
Kevin Isom <kevin.isom@gmail.com>
2015-04-23 19:22:02 -0700

    Gonna need an object for this

--- test/collections_spec.js
+++ test/collections_spec.js
@@ -1,5 +1,14 @@
+'use strict';
 import Collection from '../lib/collections';
+import assert from 'assert';
 
 describe('The Collection Object', function () {
-	
+	describe('the initial functional api', function () {
+        it('should allow an item to be added to it', function (done) {
+            let collection = new Collection();
+            collection.add({name:'my item'});
+            assert.ok(false);
+            done();
+        });  
+    });
 });
