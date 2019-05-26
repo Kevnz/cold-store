817aa6c1a4d5b593d0b4f4879fefc88f489308c7
Kevin Isom <kevin.isom@gmail.com>
2019-05-26 15:37:10 +1200

    paving the way

--- index.js
+++ index.js
@@ -1,45 +1,40 @@
-import sift from 'sift';
-import DataCollection from './lib/data-collection';
+import sift from 'sift'
+import DataCollection from './lib/data-collection'
 
 export default class ColdStore {
-    constructor(props) {
-        if(props === undefined) {
-            throw new Error('ColdStore requires at least a collection item')
-        }
-    	if (typeof props === 'string') {
-    		this.setDB = props;
-			this.collection = DataCollection.getCollection(props);
-    	} else {
-            this.setDB = props.collection;
-            this.collection = DataCollection.getCollection(props);
-        }
-    	
-
-        super(props);
+  constructor(props) {
+    if (props === undefined) {
+      throw new Error('ColdStore requires at least a collection item')
     }
-    find(query, callback) {
-    	let q;
-    	if (typeof query === 'object' && typeof callback === 'function') {
-    		let result = sift(query, this.collection.data);
-    		callback(null, result);
-    		return;
-    	}
-    	if (typeof query === 'function') {
-    		callback = query;
-    		callback(null, this.collection);
-    	} else {
-    		callback(true, null);
-    	}
-    	
-
+    if (typeof props === 'string') {
+      this.setDB = props
+      this.collection = DataCollection.getCollection(props)
+    } else {
+      this.setDB = props.collection
+      this.collection = DataCollection.getCollection(props)
     }
-    save(item, callback) {
-    	this.collection.push(item);
-    	callback(null, item);
+  }
+  find(query, callback) {
+    let q
+    if (typeof query === 'object' && typeof callback === 'function') {
+      let result = sift(query, this.collection.data)
+      callback(null, result)
+      return
     }
-    delete(query, callback) {
-    	let data = sift(query, this.collection.data);
-        this.collection.data = data;
-    	callback(null, null);
+    if (typeof query === 'function') {
+      callback = query
+      callback(null, this.collection)
+    } else {
+      callback(true, null)
     }
+  }
+  save(item, callback) {
+    this.collection.push(item)
+    callback(null, item)
+  }
+  delete(query, callback) {
+    let data = sift(query, this.collection.data)
+    this.collection.data = data
+    callback(null, null)
+  }
 }
